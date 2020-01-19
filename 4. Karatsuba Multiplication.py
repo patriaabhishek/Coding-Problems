57b9a21ed846b39331c0363f012064d54e9ccb4f
Abhishek Patria <patriaabhishek@gmail.com>
2020-01-19 12:39:24 -0500

    Create 4. Karatsuba Multiplication.py

---
+++ 4. Karatsuba Multiplication.py
@@ -0,0 +1,23 @@
+def karatsuba(x, y):
+    
+    #Getting lengths of strings
+    x_len = len(str(x))
+    y_len = len(str(y))
+    
+    #Checking for base case of both x and y being single digits
+    if ((x_len == 1) and (y_len == 1)):
+        return x*y
+    
+    #Calculating half length and the various products
+    n2 = max(x_len, y_len)//2
+    b = x%(10**(n2))
+    a = x//(10**(n2))
+    d = y%(10**(n2))
+    c = y//(10**(n2))
+    
+    #Recursive calls for multiplication
+    ac = karatsuba(a, c)
+    bd = karatsuba(b, d)
+    apb_cpd = karatsuba(a+b, c+d)
+     
+    return((ac*10**(2*n2)) + ((apb_cpd-ac-bd)*10**(n2)) + bd)
